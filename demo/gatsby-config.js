e8cfccfaf53ec0e4c5e0af868d9009852b11e335
deceqadmin <72313289+deceqadmin@users.noreply.github.com>
2020-10-10 16:58:43 -0700

    Update gatsby-config.js

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -9,8 +9,9 @@ module.exports = {
     resolve: `gatsby-theme-amplify-cognito`,
     options: {
       region: '',
-      userPoolId: '',
-      userPoolWebClientId: ''
+      userPoolId: 'us-east-2_FDpXgCgAc',
+      userPoolWebClientId: '7cnimuhr4mmhhq9vgl5lfp4rf4'
+      identityPoolId: 'us-east-2:4798a636-a31c-48f4-9111-597c31a392f3'
     },
   }]
 }
